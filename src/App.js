931978ff87ad112056ff715732c548e1e2a7d8b3
fathimasyedanoor <fnsyeda@ingenio.ca>
2021-02-09 13:11:40 -0700

    trial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import { Component } from 'react';
 import logo from './logo.svg';
+
+import Validation from "./Validation/Validation"; 
+import CharComponent from "./CharComponent/CharComponent";
 import './App.css';
 
-function App() {
+
+class App extends Component {
+
+  state = { strLen: 0, char: ''}
+
+  textLengthHandler =(event) => {
+    console.log(event);
+    const strLen = event.target.value.length;
+    const str = event.target.value;
+
+    this.setState({strLen: strLen, char: str});
+  }
+
+  deleteCharHandler = (index) => {
+    const char = [...this.state.char];
+    char.splice(index, 1);
+    const charUpdated = char.join('');
+    this.setState({char: charUpdated});
+  }
+  
+
+  render() {
+    let char = null;
+    
+    let charComp = null; 
+  if(this.state.strLen) {
+   char = [...this.state.char]
+   console.log(char);
+   charComp = 
+   (
+     <div>
+       {
+ char.map((char, index) => {
+    return <CharComponent key={index} char={char} changed={() => this.deleteCharHandler(index)}/>
+   }) 
+  }
+     </div>
+    
+   )
+  }
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="texr" onChange={this.textLengthHandler} value={this.state.char}/>
+      <p>{this.state.strLen}</p>
+      <Validation strlen={this.state.strLen}/>
+      {charComp}
     </div>
   );
+}
 }
 
 export default App;
